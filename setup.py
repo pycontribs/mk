50b03cee6e02628add19f23bfb1556866a863c0c
Sorin Sbarnea <ssbarnea@redhat.com>
2021-03-16 09:23:57 +0000

    Fixed packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 """PEP-518 backwards compatibility."""
 from setuptools import setup
 
-setup(name="mk2", use_scm_version=True, setup_requires=["setuptools_scm"])
+setup(
+    name="mk",
+    use_scm_version={"local_scheme": "no-local-version"},
+    setup_requires=["setuptools_scm[toml]>=3.5.0"],
+)
