875dfe80c1e0784feb4f38e1d10d2efc1a2fb5f9
Sorin Sbarnea <ssbarnea@redhat.com>
2021-03-16 17:21:22 +0000

    Add support for uploading changes (#8)

--- src/mk/__main__.py
+++ src/mk/__main__.py
@@ -23,9 +23,13 @@ def print_version(ctx, param, value):
 
 @click.group()
 @click.option("--version", is_flag=True, callback=print_version, expose_value=False, is_eager=True)
-def cli():
+@click.pass_context
+def cli(ctx):
     """Run cli."""
+    ctx.ensure_object(dict)
     runner = Runner()
+    ctx.obj["runner"] = runner
+
     click.secho(f"mk detected {runner.root} git repository", fg="green")
 
     # commands = {"lint": "pre-commit run -a"}
@@ -33,10 +37,21 @@ def cli():
 
 @cli.command()
 def detect():
+    """Display detected information about current project."""
     click.secho("mk detect...", fg="yellow")
 
 
 @cli.command()
 def lint():
+    """Perform linting."""
     click.secho("mk lint...", fg="yellow")
     subprocess.call(["pre-commit", "run", "-a"])
+
+
+@cli.command()
+@click.pass_context
+def up(ctx):
+    """Upload current change by creating or updating a CR/PR."""
+    click.secho("mk up...", fg="yellow")
+    ctx.obj["runner"].up()
+    # import pdb; pdb.set_trace()
--- src/mk/runner.py
+++ src/mk/runner.py
@@ -1,14 +1,53 @@
 import logging
 import git
+from pathlib import Path
+import subprocess
+import sys
 from mk.tools import Tool
 
 
 class Runner:
     def __init__(self):
         self.repo = git.Repo(".", search_parent_directories=True)
-        self.root = self.repo.working_dir
+        self.root = Path(self.repo.working_dir)
+
         for c in Tool:
             if c.is_present(self.root):
                 logging.info(f"Detected {c} !")
             else:
                 logging.debug(f"{c} not detected !")
+
+        if self.repo.is_dirty():
+            logging.warning(f"Repo is dirty on {self.repo.active_branch}")
+
+    def up(self):
+        if self.repo.is_dirty():
+            sys.exit(2)
+        if (self.root / ".git-review").is_file():
+            cmd = ["git", "review"]
+        else:
+            if self.repo.active_branch in ["main", "master"]:
+                fail(
+                    "Uploading from default branche is not allowed in order to avoid accidents.", 2
+                )
+            run(["git", "push", "--force-with-lease", "-u", "origin", "HEAD"])
+            # github for the moment
+            # https://github.com/cli/cli/issues/1718
+
+            # --web option is of not use because it happens to soon, confusing github
+            cmd = ["gh", "pr", "create", "--fill"]  # {self.repo.active_branch}
+        run(cmd)
+
+
+def fail(msg: str, code=1) -> None:
+    logging.error(msg)
+    sys.exit(code)
+
+
+def run(*args) -> None:
+    result = subprocess.run(*args, check=False)
+    if result.returncode != 0:
+        fail(
+            f"Received exit code {result.returncode} from: {' '.join(result.args)}",
+            code=result.returncode,
+        )
