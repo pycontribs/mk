a6af1b5583c22a28d879557821b4c8ad819748f6
Sorin Sbarnea <ssbarnea@redhat.com>
2021-04-10 12:11:13 +0100

    Avoid attempt to create a pull-request if one exists

--- src/mk/tools/git.py
+++ src/mk/tools/git.py
@@ -1,5 +1,8 @@
 import logging
+import os
+import re
 import sys
+import subprocess
 from typing import List, Optional
 
 from mk.exec import fail, run
@@ -31,6 +34,7 @@ class GitTool(Tool):
             sys.exit(2)
         if (runner.root / ".gitreview").is_file():
             cmd = ["git", "review"]
+            run(cmd)
         else:
             if runner.repo.active_branch in ["main", "master"]:
                 fail(
@@ -41,5 +45,26 @@ class GitTool(Tool):
             # https://github.com/cli/cli/issues/1718
 
             # --web option is of not use because it happens to soon, confusing github
-            cmd = ["gh", "pr", "create", "--fill"]  # {self.repo.active_branch}
-        run(cmd)
+            # environ={'PAGER': 'cat'}
+            logging.debug("Tryging to detect if there are existing PRs open")
+            os.environ['GITPAGER'] = 'cat'
+            result = subprocess.run(
+                ["gh", "pr", "list", "-S", f"head:{runner.repo.active_branch}"],
+                check=False,
+                stdin=subprocess.DEVNULL,
+                env=os.environ,
+                universal_newlines=True,
+                capture_output=True)
+            if result.returncode == 0:
+                print(result)
+                pr_list = []
+                for line in result.stdout.splitlines():
+                    pr_list.append(line.split('\t')[0])
+                if len(pr_list) == 0:
+                    logging.debug("Existing PR not found, creating one.")
+                    cmd = ["gh", "pr", "create", "--fill"]  # {self.repo.active_branch}
+                    run(cmd, environ={'PAGER': 'cat'})
+                elif len(pr_list) == 1:
+                    logging.debug("PR#%s already exists, no need to update.", pr_list[0])
+                else:
+                    logging.warning("Unable to decide which PR to use when multiple are found: %s", pr_list)
