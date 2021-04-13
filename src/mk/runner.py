88254e071a97d618d827ebd6b925f39e13a82d0a
Sorin Sbarnea <ssbarnea@redhat.com>
2021-04-13 11:46:47 +0100

    Add caching of actions

--- src/mk/runner.py
+++ src/mk/runner.py
@@ -1,18 +1,20 @@
 import logging
+from functools import cached_property
 from pathlib import Path
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, Optional
 
 if TYPE_CHECKING:
     from mk.tools import Action
 
 import pluggy
+from diskcache import Cache
 from git import Repo
 from git.exc import GitError
 
 
 class Runner:
     def __init__(self) -> None:
-        self.actions: List["Action"] = []
+        self.root: Optional[Path] = None
         try:
             self.repo = Repo(".", search_parent_directories=True)
         except GitError:
@@ -21,22 +23,40 @@ class Runner:
             return
 
         self.root = Path(self.repo.working_dir)
-
-        self.pm = pluggy.PluginManager("mk_tools")
-        self.pm.load_setuptools_entrypoints("mk_tools")
-        for _, cls_name in self.pm.list_name_plugin():
-            # for c in Tool:
-            c = cls_name()
-            if c.is_present(self.root):
-                logging.debug("Detected %s !", c)
-                self.actions.extend(c.actions())
-            else:
-                logging.debug("%s not detected !", c)
+        self.cache = Cache(self.root / ".cache/mk/")
 
         if self.repo.is_dirty():
             logging.warning("Repo is dirty on %s", self.repo.active_branch)
 
-        self.actions.sort()
+    @cached_property
+    def pm(self):
+        """Plugin manager."""
+        pm = pluggy.PluginManager("mk_tools")
+        pm.load_setuptools_entrypoints("mk_tools")
+        return pm
+
+    @cached_property
+    def actions(self) -> List["Action"]:
+        """List of discovered actions."""
+        if not self.root:
+            return []
+
+        _actions: List["Action"] = self.cache.get("actions", [])
+
+        if not _actions:
+            for _, cls_name in self.pm.list_name_plugin():
+                # for c in Tool:
+                c = cls_name()
+                if c.is_present(self.root):
+                    logging.debug("Detected %s !", c)
+                    _actions.extend(c.actions())
+                else:
+                    logging.debug("%s not detected !", c)
+
+        _actions.sort()
+        self.cache.set("actions", _actions, expire=3600 * 24)
+
+        return _actions
 
     def info(self) -> None:
         logging.info("Actions identified: %s", self.actions)
