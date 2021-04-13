ce9e8f05c0a5c6cded997db124224e2bcd29de7a
Sorin Sbarnea <ssbarnea@redhat.com>
2021-04-13 12:04:39 +0100

    Implement pip workaround for editable installs
    See: https://github.com/pypa/pip/issues/7953

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 """PEP-518 backwards compatibility."""
+import site
+import sys
+
 from setuptools import setup
+
+# See https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 setup(
     name="mk",
