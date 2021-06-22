d29a956fc878089f7d3e12306d2604b83c32bcce
Sorin Sbarnea <ssbarnea@redhat.com>
2021-06-22 10:25:52 +0100

    Workaround tox showconfig bug
    Related: https://github.com/tox-dev/tox/issues/2030

--- src/mk/exec.py
+++ src/mk/exec.py
@@ -1,6 +1,8 @@
 import logging
 import subprocess
 import sys
+from os import environ
+from typing import Dict, Optional
 
 import subprocess_tee
 
@@ -10,7 +12,14 @@ def fail(msg: str, code: int = 1) -> None:
     sys.exit(code)
 
 
-def run(args, *, check=False, cwd=None, tee=False) -> subprocess.CompletedProcess:
+def run(
+    args, *, check=False, cwd=None, tee=False, env_overrides: Optional[Dict[str, str]] = None
+) -> subprocess.CompletedProcess:
+    env: Optional[Dict[str, str]] = None
+    if env_overrides:
+        env = environ.copy()
+        env.update(env_overrides)
+
     if isinstance(args, str):
         cmd = args
         shell = True
@@ -19,12 +28,7 @@ def run(args, *, check=False, cwd=None, tee=False) -> subprocess.CompletedProces
         shell = False
     logging.info("Executing: %s", cmd)
     result = subprocess_tee.run(
-        args,
-        check=check,
-        shell=shell,
-        universal_newlines=True,
-        cwd=cwd,
-        tee=tee,
+        args, check=check, shell=shell, universal_newlines=True, cwd=cwd, tee=tee, environ=env
     )
     return result
 
@@ -33,9 +37,11 @@ def run_or_raise(*args, cwd=None, tee=False) -> subprocess.CompletedProcess:
     return run(*args, check=True, cwd=cwd, tee=tee)
 
 
-def run_or_fail(*args, cwd=None, tee=False) -> subprocess.CompletedProcess:
+def run_or_fail(
+    *args, cwd=None, tee=False, env_overrides: Optional[Dict[str, str]] = None
+) -> subprocess.CompletedProcess:
     try:
-        return run(*args, check=True, cwd=cwd, tee=tee)
+        return run(*args, check=True, cwd=cwd, tee=tee, env_overrides=env_overrides)
     except subprocess.CalledProcessError as exc:
         msg = f"Received exit code {exc.returncode} from: {args}"
         if not tee:
