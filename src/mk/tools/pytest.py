3e9233503ec9442429344a6bee7231682e3dd0a1
Sorin Sbarnea <ssbarnea@redhat.com>
2023-03-13 13:15:17 +0000

    Allow pytest to detect configuration from pyproject.toml (#133)

--- src/mk/tools/pytest.py
+++ src/mk/tools/pytest.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import List, Optional
 
 from mk.exec import run_or_fail
+from mk.loaders import load_toml
 from mk.tools import Action, Tool
 
 
@@ -25,6 +26,9 @@ class PyTestTool(Tool):
 
     def is_present(self, path: Path) -> bool:
         if os.path.isfile(path / "pytest.ini"):
+            return True
+        data = load_toml(path / "pyproject.toml")
+        if data and data.get("tool", {}).get("pytest"):
             return True
         return False
 
