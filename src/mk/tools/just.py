e7cbc7de290f32e60e45dc7ba3c96923530ad5bb
Edwin Kofler <edwin@kofler.dev>
2023-12-05 07:41:18 -0800

    feat: Support Just task runner

--- src/mk/tools/just.py
+++ src/mk/tools/just.py
@@ -8,41 +8,43 @@ from mk.exec import run_or_fail
 from mk.tools import Action, Tool
 
 
-class MakeTool(Tool):
-    name = "make"
+class JustTool(Tool):
+    name = "just"
 
     def __init__(self) -> None:
         super().__init__(self)
-        self.makefile: str | None = None
+        self.justfile: str | None = None
 
     def run(self, action: Action | None = None) -> None:
-        cmd = ["make"]
+        cmd = ["just"]
         if action:
             cmd.append(action.name)
         run_or_fail(cmd, tee=True)
 
     def is_present(self, path: Path) -> bool:
-        for name in ["Makefile", "makefile", "GNUmakefile"]:
-            makefile = os.path.join(path, name)
-            if os.path.isfile(makefile):
-                self.makefile = makefile
+        for name in ["Justfile", "justfile", "JUSTFILE", ".justfile"]:
+            justfile = os.path.join(path, name)
+            if os.path.isfile(justfile):
+                self.justfile = justfile
                 return True
         return False
 
     def actions(self) -> list[Action]:
         actions = []
-        if not self.makefile:
-            msg = "Makefile not found"
+        if not self.justfile:
+            msg = "Justfile not found"
             raise RuntimeError(msg)
-        with open(self.makefile, encoding="utf-8") as file:
+        # pylint: disable=duplicate-code
+        with open(self.justfile, encoding="utf-8") as file:
             for line in file.readlines():
                 # Current implementation assumes that descriptions are added
                 # using double ## after the target name.
-                # Inspired by https://github.com/containers/podman/blob/6d984739458511f444e80f06f6a12dd400268372/Makefile#L127
+                # Inspired by https://github.com/containers/podman/blob/master/Makefile#L127
                 match = re.match(r"^([a-zA-Z_-]+):.*?## (.*)$$", line)
                 if match:
                     target, description = match.groups()
                     actions.append(
                         Action(name=target, tool=self, description=description),
                     )
+        # pylint: enable=duplicate-code
         return actions
